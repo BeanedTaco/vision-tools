7b18bbe62c4612ced0afa631a5f33f863ecac807
Danny H <git@bean.codes>
2020-12-11 23:47:11 -0800

    Add admin privilege script and remove compression

--- scripts/buildall.bat
+++ scripts/buildall.bat
@@ -3,16 +3,36 @@ title Build all pkgs for Vision
 color a
 cls
 cd %~dp0
-net session >nul 2>nul
-net session >nul 2>nul
-IF %errorlevel% == 0 (
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------   
 echo The directory is: %~dp0. Correct? If so, follow the instructions below and press any key to continue.
 pause
-) else (
-echo Oops! You need to run this script in an elevated prompt. Reopen the script with admin privileges, then try again.
-pause
-exit
-)
 cls
 start cmd /c npx electron-packager . --platform=darwin --arch=x64 --out=.\dist
 start cmd /c npx electron-packager . --platform=linux --arch=x64 --out=.\dist
@@ -20,9 +40,3 @@ start cmd /c npx electron-packager . --platform=linux --arch=armv7l --out=.\dist
 start cmd /c npx electron-packager . --platform=linux --arch=arm64 --out=.\dist
 start cmd /c npx electron-builder build --win -p always
 cd %~dp0
-timeout /t 70 /nobreak
-cls
-powershell -command compress-archive -path '%CD%\dist\visionbrowser-darwin-x64' -destinationpath '%CD%\dist\visionbrowser-darwin-x64.zip'
-powershell -command compress-archive -path '%CD%\dist\visionbrowser-linux-x64' -destinationpath '%CD%\dist\visionbrowser-linux-x64.zip'
-powershell -command compress-archive -path '%CD%\dist\visionbrowser-linux-arm64' -destinationpath '%CD%\dist\visionbrowser-linux-arm64.zip'
-powershell -command compress-archive -path '%CD%\dist\visionbrowser-linux-armv7l' -destinationpath '%CD%\dist\visionbrowser-linux-armv7l.zip'
